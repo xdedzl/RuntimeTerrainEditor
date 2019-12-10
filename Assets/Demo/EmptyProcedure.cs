a2ab0242f4eb29349f5b68413d5e7bc9dfb39ea6
薛定谔的蟑螂 <xdedzl@163.com>
2019-12-10 17:30:28 +0800

    目录结构整理

---
+++ Assets/Demo/EmptyProcedure.cs
@@ -0,0 +1,1 @@
+﻿public class EmptyProcedure : XFramework.ProcedureBase { }
---
+++ Assets/Demo/TerrainModifyDemo/Scripts/TerrainModifierEntry.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using XFramework.Fsm;
+
+public class TerrainModifierEntry : MonoBehaviour
+{
+    public void Start()
+    {
+        Game.UIModule.Open(UIName.TerrainModifier);
+
+        Game.FsmModule.GetFsm<MouseFsm>().ChangeState<MouseTerrainModifierState>();
+        Game.TerrainModule.ConfigActiveTerrains();
+    }
+}
